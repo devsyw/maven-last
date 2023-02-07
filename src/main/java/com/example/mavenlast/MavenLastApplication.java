2d7821c733ab2c7ab7782a9b286cd8e26c6bcacb
devsyw <devsyw@gmail.com>
2023-02-07 20:00:12 +0900

    test code

---
+++ src/main/java/com/example/mavenlast/MavenLastApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenLastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenLastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mavenlast/MavenLastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenLastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
